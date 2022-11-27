55c7ec45277ff928b15b4b07515e3737b4f150d7
Raizen Sangalang <raizen.sangalang@hoolisoftware.com>
2022-11-27 16:46:37 +0800

    video recording explain

--- web-camera-recorder-master/camx.py
+++ web-camera-recorder-master/camx.py
@@ -1,17 +1,29 @@
 import datetime
 import cv2
 import threading
+# You can change the path to your own path
 PATH = '/home/pi/Desktop/gdrive' 
 class RecordingThread (threading.Thread):
-    def __init__(self, name, camera):
-        threading.Thread.__init__(self)
+    def __init__(self, name, camera):  
+        # A thread is use for a separate flow of execution. This means thatyour program will have two things happening at onc
+        threading.Thread.__init__(self)           
         self.name = name
         self.isRunning = True
-
+        # Open a camera
         self.cap = camera
+        # Initialize video recording environment
+        # VideoWriter_fourcc(*'MJPG') is a four character code used to specify the video codec.
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
         # make file name with timestamp format like Nov-12-2018-11-12-00
         fileName = datetime.datetime.now().strftime("%b-%d-%Y-%H-%M-%S") + ".avi"
+        # make full path with file name
+        # self.out is for recorded video
+        # VideoWriter is a class for writing video files or image sequences.
+        # 640,480 is the size of the video
+        # The First parameter is the name of the output file.
+        # The second parameter is the fourcc code
+        # The third parameter is the number of frames per second
+        # The fourth parameter is the frame size
         self.out = cv2.VideoWriter(f'{PATH}/{fileName}',fourcc, 20.0, (640,480))
 
     def run(self):
@@ -31,6 +43,7 @@ class RecordingThread (threading.Thread):
 class VideoCamera(object):
     def __init__(self):
         # Open a camera
+        # Open the first camera device
         self.cap = cv2.VideoCapture(0)
       
         # Initialize video recording environment
@@ -41,37 +54,34 @@ class VideoCamera(object):
         self.recordingThread = None
     
     def __del__(self):
+        # Release camera
+        # When everything done, release the capture
         self.cap.release()
     
     def get_frame(self):
+        # Capture frame-by-frame
         ret, frame = self.cap.read()
 
         if ret:
+            # Our operations on the frame come here
+            # imendcode() returns a memory buffer encoded as a JPEG image            
             ret, jpeg = cv2.imencode('.jpg', frame)
-
-            # Record video
-            # if self.is_record:
-            #     if self.out == None:
-            #         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-            #         self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
-                
-            #     ret, frame = self.cap.read()
-            #     if ret:
-            #         self.out.write(frame)
-            # else:
-            #     if self.out != None:
-            #         self.out.release()
-            #         self.out = None  
-
+            # Convert to bytes
+            # the memory buffer is converted to a bytes object
             return jpeg.tobytes()
       
         else:
             return None
 
     def start_record(self):
+        # Initialize video recording environment
+        # make is_record True
         self.is_record = True
+        # make recordingThread        
         self.recordingThread = RecordingThread("Video Recording Thread", self.cap)
+        # recordingThread start
         self.recordingThread.start()
+
 
     def stop_record(self):
         self.is_record = False
--- web-camera-recorder-master/server.py
+++ web-camera-recorder-master/server.py
@@ -1,4 +1,6 @@
+# use Flask to render a template, redirecting to another url, and creating a URL
 from flask import Flask, render_template, Response, jsonify, request
+# import the necessary packages
 import camx as camx2
 
 app = Flask(__name__)
@@ -6,37 +8,41 @@ app = Flask(__name__)
 video_camera_record = None
 global_framex_c = None
 
+# initialize this Flask app
 @app.route('/')
 def index():
     return render_template('recordx.html')
 
+# this route is for recording video
 @app.route('/record_status', methods=['POST'])
 def record_status():
     global video_camera_record 
+    # if the request method is POST (as by pressing record button), then read the record status
+    # if there is no video camera, create one
     if video_camera_record == None:
+        # this is the thread for recording video
         video_camera_record = camx2.VideoCamera()
-
+    # this is use to read the record status
     json = request.get_json()
-
     status = json['status']
-
+    # if the record status is true, then start recording
     if status == "true":
         video_camera_record.start_record()
         return jsonify(result="started")
     else:
         video_camera_record.stop_record()
         return jsonify(result="stopped")
-
-def video_stream():
+# This function is generate the video frames for the video recording
+def video_stream():    
     global video_camera_record 
     global global_framex_c
-
     if video_camera_record == None:
         video_camera_record = camx2.VideoCamera()
-        
-    while True:
-        frame = video_camera_record.get_frame()
+    # loop over frames from the output stream
 
+    while True:
+        # read the next frame from the stream and resize it
+        frame = video_camera_record.get_frame()
         if frame != None:
             global_framex_c = frame
             yield (b'--frame\r\n'
@@ -47,6 +53,7 @@ def video_stream():
 
 @app.route('/video_viewer')
 def video_viewer():
+    # return the response generated along with the specific media
     return Response(video_stream(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
