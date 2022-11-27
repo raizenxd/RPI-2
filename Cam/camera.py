64a0e257358297410bf048372af395a217e25338
Raizen Sangalang <raizen.sangalang@hoolisoftware.com>
2022-11-27 15:11:31 +0800

    feat: explain

--- Cam/camera.py
+++ Cam/camera.py
@@ -8,25 +8,31 @@ import numpy as np
 
 class VideoCamera(object):
     def __init__(self):
-        
-        # self.vs = WebcamVideoStream(src=0).start()
-        
+        # This code is for Windows users         
+        # self.vs = WebcamVideoStream(src=0).start()  
+        # This code is for Raspberry Pi users      
         self.vs = PiVideoStream().start()
-        
+        # This is time for camera to warm up        
         time.sleep(2.0)
     def __del__(self):
+        # code for stopping camera
         self.vs.stop()
 
     def get_frame(self):
+        # Capture frame-by-frame
         frame = self.vs.read()
         ret, jpeg = cv2.imencode('.jpg', frame)
+        # Our operations on the frame come here It will return image in binary format
         return jpeg.tobytes()
 
     def get_object(self, classifier):
+        # initialize the video stream and allow the cammera sensor to warmup        
         found_objects = False
+        # loop over the frames from the video stream
         frame = self.vs.read().copy()
+        # resize the frame, convert it to grayscale, and blur it
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+        # detect faces in the grayscale frame
         objects = classifier.detectMultiScale(
             gray,
             scaleFactor=1.1,
@@ -34,13 +40,17 @@ class VideoCamera(object):
             minSize=(30, 30),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
-
+        # if objects are found,make found_objects True
         if len(objects) > 0:
             found_objects = True
-        # OBJECT DETECTION
+
+        
+        # OBJECT DETECTION RECTANGLE DRAWING
         # for (x, y, w, h) in objects:
         #    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+
+        # show the output frame
         ret, jpeg = cv2.imencode('.jpg', frame)
         return (jpeg.tobytes(), found_objects)
 
--- Cam/mail.py
+++ Cam/mail.py
@@ -4,16 +4,14 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 import sqlite3
-# get the platform
-
+# This is the email address that will send the email
+# You will need to allow less secure apps to access this email 
+# https://myaccount.google.com/lesssecureapps 
+# https://accounts.google.com/b/0/DisplayUnlockCaptcha  
+# You can also watch this video https://youtu.be/yuT6PhH-5iw
 fromEmail = 'sekyuisme27@gmail.com'
 fromEmailPassword = 'gwlstadxritinfiq'
-
-
 toEmail = 'raizensangalang.tech@gmail.com'
-
-def getEmailSave():
-    return "dsds"
 
 def getTheEmail(USERID):
     con = sqlite3.connect('db_web.db')
@@ -25,36 +23,57 @@ def getTheEmail(USERID):
     print(records)
     return (records)
     cursor.close()
-
+# This is the SMTP mail server
+# You can use any SMTP mail server you want
+# For example, if you are using gmail, you can use smtp.gmail.com
+# smtpServer = 'smtp.gmail.com'
+# smtpPort = 587
 def sendEmail(image, USERID):
+    # Create the root message and fill in the from, to, and subject headers 
+    # MIME meaninbg Multipurpose Internet Mail Extensions
+    # https://en.wikipedia.org/wiki/MIME
+    # https://docs.python.org/3/library/email.mime.html       
     msgRoot = MIMEMultipart('related')
+    # Subject of the email
     msgRoot['Subject'] = 'Person Detected'
-    msgRoot['From'] = fromEmail
-    # msgRoot['To'] = getTheEmail()[2]
-    # print(getTheEmail()[2])
-    # #xtx = mail.getTheEmail()[1]
-    # #print(xtx)
-    # print("SDKSKDKSKDKSKD")
-    # print(getTheEmail(username))
+    # From email address
+    msgRoot['From'] = fromEmail    
+    # To email address
+    # getTheEmail is a function that will get the email address of the database
+    # The reason why it needs [3] is because the email address is the 4th column in the database    
     emailSend  =getTheEmail(USERID)[3]
+    # Print to the console the email address that will receive the email
     print(emailSend)
+    # Recipient email address
     msgRoot['To'] = emailSend
+    # Create the body of the message (a plain-text and an HTML version).
     msgAlternative = MIMEMultipart('alternative')
+    # Attach the body to the root message
     msgRoot.attach(msgAlternative)
     # get the day and time in format of Saturday - November 12, 2022 - 12:00:00 AM
     day = datetime.now().strftime("%A - %B %d, %Y - %I:%M:%S %p")
+    # Create the body of the message (a plain-text and an HTML version).
     msgText = MIMEText(f'motion detected - Date: {day}')
+    # Attach the body to the root message
     msgAlternative.attach(msgText)
-
+    # This is the message in HTML format
     msgText = MIMEText('<img src="cid:image1">', 'html')
+    # Attach the body to the root message also it is alternative format
     msgAlternative.attach(msgText)
-
+    # This example assumes the image is in the current directory
     msgImage = MIMEImage(image)
+    # Define the image's ID as referenced above
     msgImage.add_header('Content-ID', '<image1>')
+    # attach the image to the root message
     msgRoot.attach(msgImage)
-
+    # Send the email (this example assumes SMTP authentication is required)
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    # identify ourselves to smtp gmail client
+    # start TLS for security
+    # TLS (Transport Layer Security) is a cryptographic protocol that provides communication security over the Internet.
     smtp.starttls()
+    # Authentication or log in on the server
     smtp.login(fromEmail, fromEmailPassword)
+    # Send the email
     smtp.sendmail(fromEmail, emailSend, msgRoot.as_string())
     smtp.quit()
